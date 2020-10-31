0674c948db62f5d47df3b570d65aa8a278ddbba4
shuta0417 <shuta19870417@gmail.com>
2020-10-31 12:55:37 +0900

    deviseのインストール(修正)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   get 'messages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # ↑↑上の記述は削除しましょう↑↑
